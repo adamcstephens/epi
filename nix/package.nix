14eec74a46aaf6867b6e7d9fe1e2876ead5140e6
Adam C. Stephens <adam@valkor.net>
2026-03-12 20:37:55 -0400

    drop ocaml

--- nix/package.nix
+++ nix/package.nix
@@ -1,71 +1,24 @@
 {
   lib,
-  curl,
-  fetchurl,
-  makeSetupHook,
-  writeText,
-  ocamlPackages,
+  rustPlatform,
 }:
 
-let
-  duneLockHook = import ./dune-lock.nix {
-    inherit
-      lib
-      fetchurl
-      makeSetupHook
-      writeText
-      ;
-    lockDir = ../dune.lock;
-    hashOverrides = {
-      menhir = "sha512=72c85ec63104d6a7c7b6305b4642b86909fcad2e46561427ad596e37e686b3c94cd382da440f058d32800a15b157b80a8242df8043440a209ba73d209657ea84";
-      menhirCST = "sha512=72c85ec63104d6a7c7b6305b4642b86909fcad2e46561427ad596e37e686b3c94cd382da440f058d32800a15b157b80a8242df8043440a209ba73d209657ea84";
-      menhirLib = "sha512=72c85ec63104d6a7c7b6305b4642b86909fcad2e46561427ad596e37e686b3c94cd382da440f058d32800a15b157b80a8242df8043440a209ba73d209657ea84";
-      menhirSdk = "sha512=72c85ec63104d6a7c7b6305b4642b86909fcad2e46561427ad596e37e686b3c94cd382da440f058d32800a15b157b80a8242df8043440a209ba73d209657ea84";
-      ocamlfind = "sha512=8967986de2ab4ec5993f437b0a4206742adf37aa7a292a3bba0a04438d78539b84d001191e60b2d5bde98a695b38cba2593b7051f7749adbdb964a0df3c4b661";
-      otoml = "sha512=a287bf40ec7bc1458fa74bf88cd30c2f93e0092f962858fa70acdb0e01978cd187cb1135aef66a5b5775cce31353901516ecdb6822a4c29a0a99f077f30a2149";
-      re = "sha512=cd2cc39f951ca6b7be631bbb5531ed13bc040e629842671bf6fef3911b20ef1653fa9a1f0aa23b094d252cffc9a9efe7ffca69e50d362ab935bc0cc447548124";
-    };
-  };
-in
-
-ocamlPackages.buildDunePackage {
+rustPlatform.buildRustPackage {
   pname = "epi";
   version = "0.1.0";
-
-  env.BUILD_STATIC = "1";
 
   src =
     with lib.fileset;
     toSource {
       root = ../.;
       fileset = unions [
-        ../bin
-        ../dune-project
-        ../dune-workspace
-        ../dune.lock
-        ../epi.opam
-        ../lib
+        ../Cargo.toml
+        ../Cargo.lock
+        ../src
       ];
     };
 
-  nativeBuildInputs = [
-    curl
-    duneLockHook
-  ];
-
-  buildPhase = ''
-    runHook preBuild
-    dune build
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-    dune install --prefix $out
-    runHook postInstall
-  '';
-
-  passthru = { inherit duneLockHook; };
+  cargoLock.lockFile = ../Cargo.lock;
 
   meta.mainProgram = "epi";
 }
--- test/e2e/test_e2e.ml
+++
@@ -1,8 +0,0 @@
-let () =
-  Alcotest.run "epi-e2e"
-    [
-      ("e2e-setup", Test_e2e_setup.tests);
-      ("e2e-lifecycle", Test_lifecycle_e2e.tests);
-      ("e2e-mount", Test_mount_e2e.tests);
-      ("e2e-hooks", Test_hooks_e2e.tests);
-    ]
